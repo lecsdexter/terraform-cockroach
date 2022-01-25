fdfe0a2b2a737d27b911be2f9bf7859c42de61fc
luciocanche <lucio.canche@digitalonus.com>
2022-01-25 14:15:01 -0600

    testing lower size

--- modules/vm/vm.tf
+++ modules/vm/vm.tf
@@ -20,7 +20,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   name                = "vm-cockroach-${var.index}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  size                = "Standard_F2s_v2"
+  // size                = "Standard_F2s_v2"
+  size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
